02f3c028c56a69ccc5be4265e413bab318f9fb58
Clarke <clarke@HP-Laptop>
2024-09-19 10:15:03 -0400

    First Time

---
+++ src/main/java/com/clarke/microservice/config/spring_cloud_config_server/In28MinCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.clarke.microservice.config.spring_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28MinCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28MinCloudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarke/microservice/config/spring_cloud_config_server/In28MinCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarke.microservice.config.spring_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28MinCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
